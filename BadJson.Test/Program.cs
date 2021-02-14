4242c6b32750068f54b7c242d76f4d68ebf6c23d
BadRyuner <54708336+BadRyuner@users.noreply.github.com>
2021-02-14 12:44:30 +0300

    First Release

---
+++ BadJson.Test/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+
+namespace BadJson.Test
+{
+    public class Program
+    {
+        public static string myjson = System.IO.File.ReadAllText("test.json");
+
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<JsonBenchmark>();
+
+            MyBenchmark();
+
+            Console.ReadKey();
+        }
+
+        static void MyBenchmark()
+        {
+            var timer = System.Diagnostics.Stopwatch.StartNew();
+            var a = new BadJsonReader(myjson);
+            timer.Stop();
+            Console.WriteLine($"BadJson\n{timer.ElapsedMilliseconds}ms\n{a.Result["String"]}\n{string.Join(",", a.Result["String_array"])}\n{a.Result["Int"]}\n{string.Join(",", a.Result["Int_array"])}\n");
+
+            timer.Restart();
+            var b = Utf8Json.JsonSerializer.Deserialize<MyJsonStruct>(myjson);
+            timer.Stop();
+            Console.WriteLine($"utf8json\n{timer.ElapsedMilliseconds}ms\n{b.String}\n{b.String_array}\n{b.Int}\n{b.Int_Array}\n");
+        }
+
+        public class JsonBenchmark
+        {
+
+            [Benchmark]
+            public void Utf8Jsontest()
+            {
+                Utf8Json.JsonSerializer.Deserialize<MyJsonStruct>(Program.myjson);
+            }
+
+            [Benchmark]
+            public void MyJsontest()
+            {
+                new BadJsonReader(Program.myjson);
+            }
+        }
+
+        public struct MyJsonStruct
+        {
+            public string String { get; set; }
+
+            public string[] String_array { get; set; }
+
+            public int Int { get; set; }
+
+            public string[] Int_Array { get; set; }
+        }
+    }
+}
---
+++ BadJson/BadJsonReader.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BadJson
+{
+    public class BadJsonReader
+    {
+        public Dictionary<string, object> Result = new Dictionary<string, object>();
+
+        private string targetjson;
+
+        public BadJsonReader(string json)
+        {
+            targetjson = json;
+            StartJob().Wait();
+        }
+
+        private async Task StartJob()
+        {
+            List<Task> tasks = new List<Task>();
+
+            string[] json_in_lines = targetjson.Split('\n');
+
+            foreach(string json_line in json_in_lines)
+            {
+                if(json_line.Length > 2) tasks.Add(LineWorker(json_line));
+            }
+
+            foreach (Task task in tasks) await task; 
+        }
+
+        private async Task LineWorker(string line)
+        {
+            string[] TwoSide = line.Split(':');
+
+            string LeftSide = TwoSide.First();
+            string RightSide = TwoSide.Last().Replace(',', ' ');
+
+            var a = RemoveSmth(LeftSide);
+            var b = GetRightSide(RightSide); await a; await b;
+
+            LeftSide = a.Result; var rightside = b.Result;
+
+            Result.Add(LeftSide, rightside);
+
+            await Task.CompletedTask;
+        }
+
+        private async Task<string> RemoveSmth(string side)
+        {
+            var localstring = string.Empty;
+            bool quoutefound = false;
+            foreach(char c in side)
+            {
+                if (!quoutefound && c == ' ') continue;
+                if (!quoutefound && c == '"') { quoutefound = true; continue; }
+                if (quoutefound && c != '"') { localstring += c; continue; }
+                if (quoutefound && c == '"') { quoutefound = false; continue; }
+            }
+
+            await Task.CompletedTask;
+
+            return localstring;
+        }
+
+        private async Task<object> GetRightSide(string rightside)
+        {
+            await Task.CompletedTask;
+            bool quoutefound = false;
+            #region Array Object
+            if (rightside.Contains('['))
+            {
+                var obj = new List<string>();
+
+                bool arraystarted = false;
+                StringBuilder Element_From_Array = new StringBuilder();
+                foreach (var _char in rightside)
+                {
+                    if (_char == '[') { arraystarted = true; continue; }
+                    if (_char == ']') { arraystarted = false; continue; }
+
+                    if (_char == '"' && !quoutefound) { quoutefound = true; continue; }
+                    if (_char == '"' && quoutefound) { quoutefound = false; obj.Add(Element_From_Array.ToString()); Element_From_Array.Clear(); continue; }
+
+                    if (quoutefound) Element_From_Array.Append(_char);
+                }
+
+                return obj.ToArray() as object;
+            }
+            #endregion
+            #region Non-Array Object
+            else
+            {
+                StringBuilder Object = new StringBuilder();
+                #region With Quotes
+                if(rightside.Contains('"'))
+                {
+                    foreach (var _char in rightside)
+                    {
+                        if (_char == '"' && !quoutefound) { quoutefound = true; continue; }
+                        if (_char == '"' && quoutefound) { quoutefound = false; continue; }
+
+                        if (quoutefound) Object.Append(_char);
+                    }
+                }
+                #endregion
+                #region w/o
+                else
+                {
+                    foreach (var _char in rightside)
+                    {
+                        if (_char != ' ' && _char != ',') Object.Append(_char);
+                    }
+                }
+                #endregion
+                return Object.ToString() as object;
+            }
+            #endregion
+        }
+    }
+}
